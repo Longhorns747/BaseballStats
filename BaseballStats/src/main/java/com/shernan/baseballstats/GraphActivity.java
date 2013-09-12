85b4e103b0d5cd5b6823ad3d9a380303a5e7b44c
Ethan Shernan <eshernan3@gatech.edu>
2013-09-12 16:27:12 -0400

    Got a relevant and slightly dynamic graph to show up :O

--- BaseballStats/src/main/java/com/shernan/baseballstats/GraphActivity.java
+++ BaseballStats/src/main/java/com/shernan/baseballstats/GraphActivity.java
@@ -1,50 +1,62 @@
 package com.shernan.baseballstats;
 
+import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class GraphActivity extends Activity {
 
+    private String graphStats;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
-        WebView webview = (WebView) findViewById(R.id.graphView);
-        String content = "<html>"
-                + "  <head>"
-                + "    <script type=\"text/javascript\" src=\"https://www.google.com/jsapi\"></script>"
-                + "    <script type=\"text/javascript\">"
-                + "      google.load(\"visualization\", \"1\", {packages:[\"corechart\"]});"
-                + "      google.setOnLoadCallback(drawChart);"
-                + "      function drawChart() {"
-                + "        var data = google.visualization.arrayToDataTable(["
-                + "          ['Year', 'Sales', 'Expenses'],"
-                + "          ['2010',  1000,      400],"
-                + "          ['2011',  1170,      460],"
-                + "          ['2012',  660,       1120],"
-                + "          ['2013',  1030,      540]"
-                + "        ]);"
-                + "        var options = {"
-                + "          title: 'Test Data',"
-                + "          width:"
-                + "          hAxis: {title: 'Year', titleTextStyle: {color: 'red'}}"
-                + "        };"
-                + "        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));"
-                + "        chart.draw(data, options);"
-                + "      }"
-                + "    </script>"
-                + "  </head>"
-                + "  <body>"
-                + "    <div id=\"chart_div\" style=\"width:" + webview.getHeight() + "px; height: " + webview.getWidth() + "px;\"></div>"
-                + "  </body>" + "</html>";
 
-        WebSettings webSettings = webview.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webview.requestFocusFromTouch();
-        webview.loadDataWithBaseURL( "file:///android_asset/", content, "text/html", "utf-8", null );
+        Intent statsIntent = getIntent();
+        graphStats = statsIntent.getStringExtra(DisplayActivity.GRAPH_STATS);
+
+    }
+
+    public void onWindowFocusChanged(boolean hasFocus) {
+        if (hasFocus) {
+            WebView webview = (WebView) findViewById(R.id.graphView);
+
+            String content = "<html>"
+                    + "  <head>"
+                    + "    <script type=\"text/javascript\" src=\"https://www.google.com/jsapi\"></script>"
+                    + "    <script type=\"text/javascript\">"
+                    + "      google.load(\"visualization\", \"1\", {packages:[\"corechart\"]});"
+                    + "      google.setOnLoadCallback(drawChart);"
+                    + "      function drawChart() {"
+                    + "        var data = google.visualization.arrayToDataTable(["
+                    +          graphStats
+                    + "        ]);"
+                    + "        var options = {"
+                    + "          title: 'Stats!',"
+                    + "          width:" + webview.getWidth() + ","
+                    + "          height:" + webview.getHeight() + ","
+                    + "          hAxis: {title: 'Year', titleTextStyle: {color: 'red'}}"
+                    + "        };"
+                    + "        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));"
+                    + "        chart.draw(data, options);"
+                    + "      }"
+                    + "    </script>"
+                    + "  </head>"
+                    + "  <body>"
+                    + "    <div id=\"chart_div\" style=\"width:" + webview.getWidth() + "px; height: " + webview.getHeight() + "px;\"></div>"
+                    + "  </body>" + "</html>";
+
+            WebSettings webSettings = webview.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+            webview.requestFocusFromTouch();
+            webview.loadDataWithBaseURL( "file:///android_asset/", content, "text/html", "utf-8", null );
+        }
     }
 
 
@@ -53,6 +65,10 @@ public class GraphActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.graph, menu);
         return true;
+    }
+
+    public String prepareData(){
+        return null;
     }
     
 }
